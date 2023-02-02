ea4f2df0a0c3ccfcf618384ab412526c2cdd9a1a
Darya Tsukrovskaya <darya.raikhert.31@mail.ru>
2023-02-03 01:34:52 +0300

    Changes

--- src/by/teachmeskills/homework/hw_10022023/FirstHW.java
+++ src/by/teachmeskills/homework/hw_10022023/FirstHW.java
@@ -4,7 +4,7 @@ public class FirstHW {
     public static void main(String[] args) {
         int num1  = 5;
         float num2 = 12.8f;
-        int sum = num1 +(int)num2;
+        int sum = num1 - (int)num2;
         System.out.println("Cумма двух чисел равна: " + sum);
     }
 }
